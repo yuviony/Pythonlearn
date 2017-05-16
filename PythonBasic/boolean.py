cb5569e439b733521cb3b8641bc7421d4985aea5
yuviony <yuviony@gmail.com>
2017-05-16 13:37:24 +0530

    Python Boolean operations

---
+++ PythonBasic/boolean.py
@@ -0,0 +1,15 @@
+#Boolean operations
+print("and / or operations")
+print("-------------------")
+company = "Inspire"
+establish = 2001
+print("in 'and' uses both the condition is true")
+print("----------------------------------------")
+if company == "Inspire" and establish == 2001:
+    print("Inspire Techno Park LLP, Albert Complex, Tuticorin")
+print("In 'or' uses any one condition is true")
+print("--------------------------------------")
+if company == "cbm" or establish == 2001:
+    print("CBM IT Solutions, Malaysia")
+
+
---
+++ PythonBasic/inboolean.py
@@ -0,0 +1,5 @@
+#In Operations
+print("if in Statement")
+company = "Inspire"
+if company in ["Inspire","CBM"]:
+    print("Inspire Techno Park LLP")
