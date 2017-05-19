4d0032f28a4ae819597f7ddd9021b3c3c0dcf9e1
Krish V <vnkrish90@gmail.com>
2017-05-19 19:02:13 +0530

    Modified the Function.py file

--- PythonBasic/function.py
+++ PythonBasic/function.py
@@ -1,2 +1,9 @@
-def fun():
-    print("Hello Function")
+def my_first_function():
+	print("Hello, this is My first Function!")
+
+def my_function_with_args(username, msg):
+	print("Hello, Mr.%s, I Wish You %s" % (username, msg))
+	
+my_first_function()
+
+my_function_with_args("Krish John", "Happy Day!")
